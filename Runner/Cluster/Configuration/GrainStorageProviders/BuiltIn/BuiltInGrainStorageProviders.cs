6db854acd160d3717f77bdddda19f1edceed237e
Sam Fields <smfields896@gmail.com>
2023-12-21 20:49:37 -0500

    Remove duplicates

--- Runner/Cluster/Configuration/GrainStorageProviders/BuiltIn/BuiltInGrainStorageProviders.cs
+++ Runner/Cluster/Configuration/GrainStorageProviders/BuiltIn/BuiltInGrainStorageProviders.cs
@@ -1,14 +1,9 @@
-﻿using Runner.Cluster.Configuration.GrainStorageProviders.Microsoft_Orleans_Persistence_AdoNet;
-using Runner.Cluster.Configuration.GrainStorageProviders.Microsoft_Orleans_Persistence_Redis;
-
-namespace Runner.Cluster.Configuration.GrainStorageProviders.BuiltIn;
+﻿namespace Runner.Cluster.Configuration.GrainStorageProviders.BuiltIn;
 
 public static class BuiltInGrainStorageProviders
 {
     public static IEnumerable<IClusterParameter> All => 
     [
-        new MemoryGrainStorageProvider(),
-        new RedisGrainStorageProvider(),
-        new PostgresGrainStorageProvider()
+        new MemoryGrainStorageProvider()
     ];
 }
