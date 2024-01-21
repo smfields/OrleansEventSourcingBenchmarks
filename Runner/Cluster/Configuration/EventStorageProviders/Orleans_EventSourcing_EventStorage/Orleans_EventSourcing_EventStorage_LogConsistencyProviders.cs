103610615c35f49413760aebc471245fdebcdb51
Sam Fields <smfields896@gmail.com>
2024-01-21 10:02:00 -0500

    Remove marten and update packages

--- Runner/Cluster/Configuration/EventStorageProviders/Orleans_EventSourcing_EventStorage/Orleans_EventSourcing_EventStorage_LogConsistencyProviders.cs
+++ Runner/Cluster/Configuration/EventStorageProviders/Orleans_EventSourcing_EventStorage/Orleans_EventSourcing_EventStorage_LogConsistencyProviders.cs
@@ -1,6 +1,5 @@
 ﻿using Runner.Cluster.Configuration.EventStorageProviders.Orleans_EventSourcing_EventStorage.BuiltIn;
 using Runner.Cluster.Configuration.EventStorageProviders.Orleans_EventSourcing_EventStorage.EventStore;
-using Runner.Cluster.Configuration.EventStorageProviders.Orleans_EventSourcing_EventStorage.Marten;
 
 namespace Runner.Cluster.Configuration.EventStorageProviders.Orleans_EventSourcing_EventStorage;
 
@@ -9,7 +8,6 @@ public static class Orleans_EventSourcing_EventStorage_LogConsistencyProviders
     public static IEnumerable<IClusterParameter> All =>
     [
         new MemoryEventStorageProvider(),
-        new MartenEventStorageProvider(),
         new EventStoreEventStorageProvider()
     ];
 }
