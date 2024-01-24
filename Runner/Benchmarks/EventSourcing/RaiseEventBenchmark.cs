0a9a2eb7b0cd7257247847322ed8cf3c95e5535d
Sam Fields <smfields896@gmail.com>
2024-01-23 22:10:10 -0500

    Update benchmarks

--- Runner/Benchmarks/EventSourcing/RaiseEventBenchmark.cs
+++ Runner/Benchmarks/EventSourcing/RaiseEventBenchmark.cs
@@ -1,58 +1,40 @@
-﻿// using BenchmarkDotNet.Attributes;
-// using Bogus;
-// using Runner.Cluster;
-// using Runner.Configuration;
-// using Runner.Grains;
-//
-// namespace Runner.Benchmarks.RaiseEventBenchmarks;
-//
-// public abstract class RaiseEventBenchmark
-// {
-//     [Params(1, 10, 100, 1_000, Priority = -100)]
-//     public int NumEvents { get; set; }
-//     
-//     [ParamsAllValues(Priority = 100)]
-//     public bool ConfirmEachEvent { get; set; }
-//     
-//     [ParamsAllValues(Priority = 101)]
-//     public bool Reentrant { get; set; }
-//     
-//     private BenchmarkCluster TestCluster { get; set; } = null!;
-//     
-//     [GlobalSetup]
-//     public async Task GlobalSetup()
-//     {
-//         TestCluster = await TestClusterFactory.CreateTestCluster(GetClusterParameters().ToArray());
-//     }
-//
-//     [GlobalCleanup]
-//     public async Task GlobalCleanup()
-//     {
-//         await TestCluster.DisposeAsync();
-//     }
-//     
-//     [Benchmark]
-//     public async ValueTask RaiseEvents()
-//     {
-//         var grain = TestCluster.GrainFactory.GetGrain<IDataGrain>(
-//             Guid.NewGuid(),
-//             grainClassNamePrefix: Reentrant ? "Runner.Grains.DataGrainReentrant" : null
-//         );
-//
-//         var updatedText = new Faker().Lorem.Paragraph(5);
-//
-//         var tasks = Enumerable
-//             .Range(0, NumEvents)
-//             .Select(_ => grain.Update(updatedText, ConfirmEachEvent))
-//             .Select(valueTask => valueTask.AsTask());
-//         
-//         await Task.WhenAll(tasks);
-//
-//         if (!ConfirmEachEvent)
-//         {
-//             await grain.ConfirmEvents();
-//         }
-//     }
-//     
-//     protected abstract List<IConfigurationParameter> GetClusterParameters();
-// }
+﻿using BenchmarkDotNet.Attributes;
+using Bogus;
+using Runner.Grains;
+
+namespace Runner.Benchmarks.EventSourcing;
+
+public class RaiseEventBenchmark : OrleansBenchmark
+{
+    [Params(1, 10, 100, 1_000, Priority = -100)]
+    public int NumEvents { get; set; }
+    
+    [ParamsAllValues(Priority = 100)]
+    public bool ConfirmEachEvent { get; set; }
+    
+    [ParamsAllValues(Priority = 101)]
+    public bool Reentrant { get; set; }
+    
+    [Benchmark]
+    public async ValueTask RaiseEvents()
+    {
+        var grain = Cluster.GrainFactory.GetGrain<IDataGrain>(
+            Guid.NewGuid(),
+            grainClassNamePrefix: Reentrant ? "Runner.Grains.DataGrainReentrant" : null
+        );
+
+        var updatedText = new Faker().Lorem.Paragraph(5);
+
+        var tasks = Enumerable
+            .Range(0, NumEvents)
+            .Select(_ => grain.Update(updatedText, ConfirmEachEvent))
+            .Select(valueTask => valueTask.AsTask());
+        
+        await Task.WhenAll(tasks);
+
+        if (!ConfirmEachEvent)
+        {
+            await grain.ConfirmEvents();
+        }
+    }
+}
--- Runner/Benchmarks/Loading/LoadGrainBenchmark.cs
+++ Runner/Benchmarks/Loading/LoadGrainBenchmark.cs
@@ -2,12 +2,16 @@
 using Bogus;
 using Runner.Grains;
 
-namespace Runner.Benchmarks.EventSourcing;
+namespace Runner.Benchmarks.Loading;
 
 public class LoadGrainBenchmark : OrleansBenchmark
 {
-    // [Params(1, 10, 100, 1_000, Priority = -100)]
-    public int NumEvents { get; set; } = 1;
+    [Params(1, 10, 100, 1_000)]
+    public int NumUpdates { get; set; }
+    
+    [Params(10, 1_000)]
+    public int UpdateSize { get; set; }
+    
     private Guid ExistingGrainId { get; } = Guid.NewGuid();
 
     public override async Task GlobalSetup()
@@ -15,10 +19,10 @@ public class LoadGrainBenchmark : OrleansBenchmark
         await base.GlobalSetup();
         
         // Setup existing grain
-        var updatedText = new Faker().Lorem.Paragraph(5);
+        var updatedText = new Faker().Lorem.Sentences(UpdateSize);
         var grain = Cluster.GrainFactory.GetGrain<IDataGrain>(ExistingGrainId);
         var tasks = Enumerable
-            .Range(0, NumEvents)
+            .Range(0, NumUpdates)
             .Select(_ => grain.Update(updatedText, true))
             .Select(valueTask => valueTask.AsTask());
         await Task.WhenAll(tasks);
